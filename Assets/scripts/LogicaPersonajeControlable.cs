2ab5accd54fc66e43503cd023de34f4235a48c96
ALBERTO DELGADO SOLER <alu0100889772@ull.edu.es>
2020-06-25 11:11:55 +0000

    Adiciones: - Animación 'idle' cuando los alumnos están sentados - Animación 'idle' profesora - Modelo bola de papel - Script lanzamiento bolas de papel

--- Assets/scripts/LogicaPersonajeControlable.cs
+++ Assets/scripts/LogicaPersonajeControlable.cs
@@ -20,12 +20,18 @@ public class LogicaPersonajeControlable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Guardamos lo registrado en las teclas AD en la variable 'x' que toma valores -1, 0 o 1 
+        // dependiendo de cual pulsemos o si no pulsamos ninguna y lo  mismo con la variable 'y' 
+        // con las teclas WS.
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
 
+        // Rotamos dependiendo de la variable x en el eje y de rotación y avanzamos dependiendo 
+        // de la variable y en el eje z de posición.
         transform.Rotate(0, x * Time.deltaTime * velocidadGiro, 0);
         transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
 
+        // Enviamos las variables x e y al animador para ver que animación ejecutar.
         anim.SetFloat("VelX", x);
         anim.SetFloat("VelY", y);
     }
---
+++ Assets/scripts/ThrowPaperBall.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowPaperBall : MonoBehaviour
+{
+
+    public float thrust = 15.0f;
+    private Rigidbody ballRigidBody;
+    public GameObject originalBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Bola que se va a clonar para lanzarse
+        originalBall = GameObject.Find("paper_low");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Transform transf = GetComponent<Transform>();
+
+            // Copia de la bola que se crea en frente del protagonista
+            GameObject newBall = Instantiate(originalBall, new Vector3(transf.position.x + 1 * Mathf.Sin(transf.rotation.eulerAngles.y * Mathf.Deg2Rad), transf.position.y + 0.8f, transf.position.z + 1 * Mathf.Cos(transf.rotation.eulerAngles.y * Mathf.Deg2Rad)), Quaternion.identity);
+
+            ballRigidBody = newBall.GetComponent<Rigidbody>();
+
+            // Lanzamiento de la bola hacia donde mira el personaje
+
+            float direccionX = Mathf.Sin(transf.rotation.eulerAngles.y * Mathf.Deg2Rad);
+            float direccionZ = Mathf.Cos(transf.rotation.eulerAngles.y * Mathf.Deg2Rad);
+
+            ballRigidBody.AddForce(thrust * direccionX, 0, thrust * direccionZ, ForceMode.Impulse);
+        }
+    }
+}
