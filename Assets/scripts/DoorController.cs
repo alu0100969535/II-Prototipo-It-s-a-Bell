d6f97bec72e453abe368f4dbde9eb710a5ccdef0
Felipe Andrés Álvarez Avaria <alu0100969535@ull.edu.es>
2020-07-02 11:22:21 +0000

    Añadimos puertas con animación. Controlador de eventos para el jugador.

---
+++ Assets/scripts/DoorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    private bool isOpen = false;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameEvents.controller.eventHandler += onDoorInteraction;
+        animator = gameObject.transform.parent.GetComponent<Animator>();
+    }
+
+    private void onDoorInteraction(GameObject target) {
+        if(target == gameObject) {
+            isOpen = !isOpen;
+            animator.Play(isOpen ? "open" : "close");    
+        }
+    }
+}
---
+++ Assets/scripts/GameEvents.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents controller;
+    public delegate void raycastHitCheck(GameObject target);
+	public event raycastHitCheck eventHandler;
+
+    public float maxDistance = 3.0f;
+
+    private void Awake() {
+        if(controller == null){
+            controller = this;
+			DontDestroyOnLoad(this);
+		}		
+		else if(controller != this){
+			Destroy(this);
+		}
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Use Key")) {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, maxDistance))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                GameObject target = hit.transform.gameObject;
+                eventHandler(target);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            }
+        }
+    }
+}
