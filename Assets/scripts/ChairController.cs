afccabb79c9b6d13ac53ac643c7ac95eb9d5f627
Felipe Andrés Álvarez Avaria <alu0100969535@ull.edu.es>
2020-07-04 12:03:30 +0000

    Animación de la silla de la profesora + movemos un poco las sillas

---
+++ Assets/scripts/ChairController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairController : MonoBehaviour
+{
+    private bool isIn = true;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameEvents.controller.eventHandler += onChairInteraction;
+        animator = GetComponent<Animator>();
+    }
+
+    private void onChairInteraction(GameObject target) {
+        if(target == gameObject) {
+            isIn = !isIn;
+            animator.Play(isIn ? "chairIn" : "chairOut");
+        }
+    }
+}
