18c1aae6b5ba8eb997a24c47eac5e413b46e79c2
ALBERTO DELGADO SOLER <alu0100889772@ull.edu.es>
2020-07-04 15:29:21 +0000

    Añadido hud y varias animaciones

---
+++ Assets/scripts/RotateTeacher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTeacher : MonoBehaviour
+{
+
+    private Animator anim;
+    public bool facingBoard = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        facingBoard = anim.GetBool("Rotate");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int rnd = Random.Range(0, 5);
+        if (rnd == 0)
+        {
+            anim.SetTrigger("Rotate");
+        }
+        if (anim.GetBool("Rotate"))
+        {
+            anim.SetTrigger("Rotate");
+        }
+    }
+}
