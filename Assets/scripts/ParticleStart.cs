8074d60bbe45e2255a85cedb40b2cff74c9fb951
Felipe Andrés Álvarez Avaria <alu0100969535@ull.edu.es>
2020-07-06 17:46:45 +0000

    Enemigo2 suelta particulas cuando le damos con la bola de papel

---
+++ Assets/scripts/ParticleStart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleStart : MonoBehaviour
+{
+
+    public ParticleSystem hit;
+    public GameObject g;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hit = GetComponent<ParticleSystem>();
+        g = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.CompareTag("Bullet")) {
+            hit.Play();
+        }
+    }
+}
