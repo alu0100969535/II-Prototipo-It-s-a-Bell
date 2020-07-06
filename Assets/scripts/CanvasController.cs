e5ca2d4d6ba62ddbdcc160ce797324797f8b0235
Felipe Andrés Álvarez Avaria <alu0100969535@ull.edu.es>
2020-07-06 19:17:56 +0000

    Sonidos para derrota y victoria

--- Assets/scripts/CanvasController.cs
+++ Assets/scripts/CanvasController.cs
@@ -9,22 +9,48 @@ public class CanvasController : MonoBehaviour
     private GameObject gameOver;
     private GameObject youWin;
 
+    private AudioSource audioSource;
+    public AudioClip onLose;
+    public AudioClip onWin;
+
+    public int enemies = 6;
+
     // Start is called before the first frame update
     void Start()
     {
         gameplay = gameObject.transform.Find("Gameplay").gameObject;
         gameOver = gameObject.transform.Find("GameOver").gameObject;
         youWin = gameObject.transform.Find("YouWin").gameObject;
+        audioSource = GameObject.Find("EventSystem").GetComponent<AudioSource>();
+        //onLose and onWin must be initialized in Editor
+    }
 
+    private void stopBackgroundMusic() {
+        audioSource.Stop();
+    }
+
+    private void setMusic(AudioClip clip) {
+        audioSource.PlayOneShot(clip);
     }
 
     public void setGameOver() {
         gameplay.SetActive(false);
         gameOver.SetActive(true);
+        stopBackgroundMusic();
+        setMusic(onLose);
+    }
+
+    public void onEnemyDead(){
+        enemies--;
+        if(enemies == 0) {
+            setYouWin();
+        }
     }
 
     public void setYouWin() {
         gameplay.SetActive(false);
         youWin.SetActive(true);
+        stopBackgroundMusic();
+        setMusic(onWin);
     }
 }
