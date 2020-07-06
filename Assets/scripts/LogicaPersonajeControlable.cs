7daddffe2ded02c70311c736b97289cdb3687460
ALBERTO DELGADO SOLER <alu0100889772@ull.edu.es>
2020-07-06 19:37:51 +0000

    No Comments

--- Assets/scripts/LogicaPersonajeControlable.cs
+++ Assets/scripts/LogicaPersonajeControlable.cs
@@ -7,8 +7,12 @@ public class LogicaPersonajeControlable : MonoBehaviour
     public float velocidadMovimiento = 10.0f;
     public float velocidadGiro = 200.0f;
     private Animator anim;
-    public float x;
-    public float y;
+    public float HorizontalAxis;
+    public float VerticalAxis;
+    public GameObject camera;
+
+    public float alturaCamara = 0.79f;
+    public float ejeZCamara = -0.03999739f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,22 +28,41 @@ public class LogicaPersonajeControlable : MonoBehaviour
         // Guardamos lo registrado en las teclas AD en la variable 'x' que toma valores -1, 0 o 1 
         // dependiendo de cual pulsemos o si no pulsamos ninguna y lo  mismo con la variable 'y' 
         // con las teclas WS.
-        x = Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
+        HorizontalAxis = Input.GetAxis("Horizontal");
+        VerticalAxis = Input.GetAxis("Vertical");
 
         // Rotamos dependiendo de la variable x en el eje y de rotación y avanzamos dependiendo 
         // de la variable y en el eje z de posición.
-        transform.Rotate(0, x * Time.deltaTime * velocidadGiro, 0);
-        //transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+        //transform.Rotate(0, x * Time.deltaTime * velocidadGiro, 0);
+        //Quaternion rotateTo = new Quaternion(0,camera.transform.rotation.y,0,camera.transform.rotation.w);
+        //transform.Rotate(0,rotateTo.y-transform.rotation.y,0);
 
-        float direccionX = Mathf.Sin(transform.rotation.eulerAngles.y * Mathf.Deg2Rad);
-        float direccionZ = Mathf.Cos(transform.rotation.eulerAngles.y * Mathf.Deg2Rad);
+        if(camera!=null)
+            transform.eulerAngles = new Vector3(0,camera.transform.eulerAngles.y,0);
+
+        //camera.transform.rotation = new Quaternion(0, 0, 0, 1);
+        //transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+        /*
+        float direccionXForwarwd = Mathf.Sin(transform.rotation.eulerAngles.y * Mathf.Deg2Rad);
+        float direccionZForward = Mathf.Cos(transform.rotation.eulerAngles.y * Mathf.Deg2Rad);
         GetComponent<Rigidbody>().AddForce(y * Time.deltaTime * velocidadMovimiento * direccionX, 0, y * Time.deltaTime * velocidadMovimiento * direccionZ, ForceMode.Impulse);
+
+        float direccionXSideways = Mathf.Sin(transform.rotation.eulerAngles.y * Mathf.Deg2Rad);
+        float direccionZSideways = Mathf.Cos(transform.rotation.eulerAngles.y * Mathf.Deg2Rad);
+        GetComponent<Rigidbody>().AddForce(x * Time.deltaTime * velocidadMovimiento * direccionX, 0, x * Time.deltaTime * velocidadMovimiento * direccionZ, ForceMode.Impulse);
+        */
+        float vertical = VerticalAxis * velocidadMovimiento * Time.deltaTime;
+        float horizontal = HorizontalAxis * velocidadMovimiento * Time.deltaTime;
+        float z = 0;
+        //this.transform.Translate(horizontal, 0, vertical);
+        GetComponent<Rigidbody>().AddRelativeForce(horizontal, z, vertical);
+
+        camera.transform.position = new Vector3(transform.position.x, transform.position.y + alturaCamara, transform.position.z + ejeZCamara);
         //GetComponent<Rigidbody>().AddForce(velocidadMovimiento * direccionX, 0, velocidadMovimiento * direccionZ, ForceMode.Impulse);
 
         // Enviamos las variables x e y al animador para ver que animación ejecutar.
-        anim.SetFloat("VelX", x);
-        anim.SetFloat("VelY", y);
+        anim.SetFloat("VelX", HorizontalAxis);
+        anim.SetFloat("VelY", VerticalAxis);
     }
 
     
