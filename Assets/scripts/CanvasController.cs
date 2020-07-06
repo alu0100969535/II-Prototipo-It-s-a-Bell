0d8da45fdb3a499fbcb0398d77428aa54c0d6aa7
Felipe Andrés Álvarez Avaria <alu0100969535@ull.edu.es>
2020-07-06 18:02:48 +0000

    GameOver cuando tenemos 0 vidas

---
+++ Assets/scripts/CanvasController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+
+    private GameObject gameplay;
+    private GameObject gameOver;
+    private GameObject youWin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameplay = gameObject.transform.Find("Gameplay").gameObject;
+        gameOver = gameObject.transform.Find("GameOver").gameObject;
+        youWin = gameObject.transform.Find("YouWin").gameObject;
+
+    }
+
+    public void setGameOver() {
+        gameplay.SetActive(false);
+        gameOver.SetActive(true);
+    }
+
+        public void setYouWin() {
+        gameplay.SetActive(false);
+        youWin.SetActive(true);
+    }
+}
